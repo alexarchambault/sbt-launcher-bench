9e2eadcf080bbae6e5d2cd71f75c2fa211e1f239
Alexandre Archambault <alexandre.archambault@gmail.com>
2018-10-25 10:40:21 +0200

    Add coursier sbt-launcher for comparison
    https://github.com/coursier/sbt-launcher

--- run-clean.sh
+++ run-clean.sh
@@ -4,6 +4,7 @@ function prepare {
   rm -rf ./target
   rm -rf ./sbt
   rm -rf ./ivy2
+  rm -rf ./coursier
 
   mkdir project
   echo "sbt.version=1.1.1" > project/build.properties
@@ -13,8 +14,19 @@ function run {
   time sbt -sbt-jar $1 -ivy ./ivy2 -sbt-dir ./sbt exit > /dev/null 2> /dev/null
 }
 
+function runCsbt {
+  time COURSIER_CACHE="$(pwd)/coursier" ./csbt exit > /dev/null 2> /dev/null
+}
+
 for i in {1..3}
 do
+  prepare
+  echo "csbt"
+  runCsbt
+
+  echo
+  echo
+
   prepare
   echo "default"
   run sbt-launch-1.0.4-SNAPSHOT-default.jar
